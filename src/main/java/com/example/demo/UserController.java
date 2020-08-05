e4bab8db716a42c8a6744acd6b03a278731676a5
veeraphat-amo <veeraphat_amo@hotmail.com>
2020-08-05 12:55:41 +0700

    Step_02 query message

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,17 +1,31 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
     @GetMapping("/users")
-    public UserResponse[] getAllUser() {
-        UserResponse[] userResponses = new UserResponse[2];
-        userResponses[0] = new UserResponse(1, "User 1");
-        userResponses[1] = new UserResponse(2, "User 2");
-        return userResponses;
+    public List<UserResponse> getAllUser(
+            @RequestParam(required = false, defaultValue ="1") int page,
+            @RequestParam(required = false, defaultValue = "30") int item) {
+
+        List<UserResponse> usersResponseList = new ArrayList<>();
+        for(int i=1;i<=item;i++){
+            usersResponseList.add(new UserResponse(i, "User "+i+", page: "+page));
+        }
+        return usersResponseList;
+    }
+
+    @GetMapping("/users/{id}")
+    public UserResponse getUserById(@PathVariable int id) {
+        return new UserResponse(id, "User " + id);
     }
 
 }
